56502335d934c8de2cd79eb310fecfa7a905a144
Rafael Alapont Gómez <rafael.alapont.gomez@gmail.com>
2025-01-05 22:05:26 +0100

    secuencia

--- database/sampdb.sql
+++ database/sampdb.sql
@@ -1,3 +1,18 @@
+-- SEQUENCE: public.president_id_seq
+
+-- DROP SEQUENCE IF EXISTS public.president_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS public.president_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1
+    OWNED BY president.id;
+
+ALTER SEQUENCE public.president_id_seq
+    OWNER TO postgres;
+
 -- Table: public.president
 
 -- DROP TABLE IF EXISTS public.president;
